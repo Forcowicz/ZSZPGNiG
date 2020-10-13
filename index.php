bc726eae091e3c5cd4b275d7ab4bc38fc9ec2e3b
Forcowicz <forcowicz@gmail.com>
2020-10-13 23:24:37 +0200

    Navbar components

--- index.php
+++ index.php
@@ -12,15 +12,97 @@
 </head>
 <body>
 <nav class="nav">
-    <ul class="nav__list">
-        <li class="nav__item"><a href="#" class="nav__link">Szkoła</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Szkolne życie</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Dla uczniów</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Rekrutacja</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Dla rodziców</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Projekty edukacyjne</a></li>
-        <li class="nav__item"><a href="#" class="nav__link">Erasmus</a></li>
-    </ul>
+    <div class="nav__list">
+        <div class="nav__item">
+            <button class="nav__button"><a href="#" class="nav__link">Szkoła</a></button>
+            <div class="nav__dropdown">
+                <ul class="nav__dropdown-list">
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown" id="kkk">O szkole</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Dyrekcja</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Sekretariat</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Grono pedagogiczne</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Statut ZSZ PGNiG</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Wirtualny spacer po szkole</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Kalendarz roku szkolnego</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Nasi partnerzy</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Ewaluacja - raport o szkole</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Szkoła policealna</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav__item">
+            <button class="nav__button"><a href="#" class="nav__link">Szkolne życie</a></button>
+            <div class="nav__dropdown">
+                <ul class="nav__dropdown-list">
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav__item">
+            <button class="nav__button"><a href="#" class="nav__link">Dla uczniów</a></button>
+            <div class="nav__dropdown">
+                <ul class="nav__dropdown-list">
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav__item">
+            <button class="nav__button"><a href="#" class="nav__link">Rekrutacja</a></button>
+            <div class="nav__dropdown">
+                <ul class="nav__dropdown-list">
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav__item">
+            <button class="nav__button"><a href="#" class="nav__link">Dla rodziców</a></button>
+            <div class="nav__dropdown">
+                <ul class="nav__dropdown-list">
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav__item">
+            <button class="nav__button"><a href="#" class="nav__link">Projekty edukacyjne</a></button>
+            <div class="nav__dropdown">
+                <ul class="nav__dropdown-list">
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="nav__item">
+            <button class="nav__button"><a href="#" class="nav__link">Erasmus</a></button>
+            <div class="nav__dropdown">
+                <ul class="nav__dropdown-list">
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </nav>
 <header class="header">
     <div class="header__heading-box">
