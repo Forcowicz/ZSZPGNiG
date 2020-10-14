fa0dba224a5c439ccb74387be532ff0456d15c73
Forcowicz <forcowicz@gmail.com>
2020-10-14 05:32:34 +0200

    Expanded navbar

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <button class="nav__button"><a href="#" class="nav__link">Szkoła</a></button>
             <div class="nav__dropdown">
                 <ul class="nav__dropdown-list">
-                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown" id="kkk">O szkole</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">O szkole</a></li>
                     <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Dyrekcja</a></li>
                     <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Sekretariat</a></li>
                     <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Grono pedagogiczne</a></li>
@@ -34,14 +34,28 @@
             <button class="nav__button"><a href="#" class="nav__link">Szkolne życie</a></button>
             <div class="nav__dropdown">
                 <ul class="nav__dropdown-list">
-                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
-                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
-                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
-                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
-                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Wycieczki</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Nasze pasje</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Fotorelacje</a></li>
+                    <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Ćwiczenia terenowe</a></li>
+                    <li class="nav__dropdown-item nav__dropdown-item--expand">
+                        <a href="#" class="nav__link nav__link--dropdown">
+                            <svg class="nav__dropdown-icon">
+                                <use xlink:href="assets/icons/sprite.svg#double-right-arrow"></use>
+                            </svg>
+                            Aktualności
+                        </a>
+
+                        <ul class="nav__dropdown--expand">
+                            <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                            <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                            <li class="nav__dropdown-item"><a href="#" class="nav__link nav__link--dropdown">Placeholder</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
         </div>
+        <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> -->
         <div class="nav__item">
             <button class="nav__button"><a href="#" class="nav__link">Dla uczniów</a></button>
             <div class="nav__dropdown">
